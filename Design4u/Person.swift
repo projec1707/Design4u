74a63035389609f903b640d49ca685d366937ee0
projec1707 <pwroma@gmail.com>
2023-05-03 18:01:39 +0300

    Create struct Person

--- Design4u/Person.swift
+++ Design4u/Person.swift
@@ -8,10 +8,56 @@ struct Person: Comparable, Decodable, Identifiable {
     var thumbnail: URL
     var firstName: String
     var lastName: String
+    var email: String
     var experience: Int
     var rate: Int
     var bio: String
     var details: String
     var skills: Set<Skill>
     var tags: [String]
+    
+    var displayName: String {
+        let components = PersonNameComponents(givenName: firstName, familyName: lastName)
+        return components.formatted()
+    }
+    
+    static func <(lhs: Person, rhs: Person) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+    
+    static let example = Person(
+        id: 1,
+        photo: URL(string: "https://hws.dev/img/user-1-full.jpg")!,
+        thumbnail: URL(string: "https://hws.dev/img/user-1-thumb.jpg")!,
+        firstName: "Jaime",
+        lastName: "Rove",
+        email: "jrove1@huffingtonpost.com",
+        experience: 10,
+        rate: 300,
+        bio: "I am a graphic designer who specializes in creating unique and eye-catching logos.",
+        details: "My go-to tools are Adobe Illustrator and Photoshop. I love designing anything that can make a strong impact and leave a lasting impression.",
+        skills: [Skill("Illustrator"), Skill("Photoshop")],
+        tags: [
+            "ideator",
+            "aligned",
+            "manager",
+            "excitable"
+         ])
+}
+
+struct Skill: Comparable, Decodable, Hashable, Identifiable {
+    var id: String
+    
+    init(_ id: String) {
+        self.id = id
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        self.id = try container.decode(String.self)
+    }
+    
+    static func <(lhs: Skill, rhs: Skill) -> Bool {
+        lhs.id < rhs.id
+    }
 }
